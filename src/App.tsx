a1931a8e4cdc41824b132df9a2bfe984ccce5663
pakdipong <157682440+pakdipong@users.noreply.github.com>
2024-05-19 18:31:36 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useForm } from "react-hook-form"
+import './App.css'
+import { useState } from "react"
+
+type Inputs = {
+  weight: number
+}
+
+function App() {
+  const { register, handleSubmit } = useForm<Inputs>()
+  const [result, setResult] = useState<number>(0)
+
+  const waterCal = (data: any) => {
+    setResult(Math.floor(+data.weight * 2.2 * 30 / 2))
+  }
+
+  return (
+    <>
+      <h1>ควรดื่มน้ำวันละเท่าไหร่ ?</h1>
+      <h1>{result} มล.</h1>
+      <form onSubmit={handleSubmit(waterCal)}>
+        <input {...register('weight')} type="number" placeholder='น้ำหนังของคุณ (กิโลกรัม)' />
+        <br />
+        <br />
+        <input type="submit" value="คำนวน" />
+      </form>
+    </>
+  )
+}
+
+export default App
